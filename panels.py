1882c250d16fa3890e640d59653575a3621cdcd0
Mohammed Al-Mahdawi <48254077+Mohido@users.noreply.github.com>
2024-03-05 22:45:09 +0100

    Implemented Mapping Logic (#1)
    * Add/Remove Expressions, Add/Remove Node Labels
    * Assigning Logic Works Perfectly
    * Finished Logic Implementation. Tested It Through UI

--- panels.py
+++ panels.py
@@ -27,14 +27,37 @@ class ImageMapperPanel(bpy.types.Panel):
 
         layout.prop(props, "object_name_pattern")
         layout.prop(props, "general_material")
-        layout.prop(props, "mapping_expression")
         layout.prop(props, "nested_node_search")
         layout.prop(props, "cleanup_copied_materials")
 
+        layout.label(text="Expressions:")
+        expBox = None
+        for i, expression in enumerate(props.expressions):
+            if(not expBox):
+                expBox = layout.box()
+            row = expBox.row()
+            row.prop(expression, "expression", text="")
+            row.operator("image_mapper.remove_expression", icon='X', text="").index = i
+        layout.operator("image_mapper.add_expression", icon='ADD', text="Add Expression")
+
+
+        layout.label(text="Image Texture Node Labels:")
+        labelBox = None
+        for i, label in enumerate(props.image_node_labels):
+            if(not labelBox):
+                labelBox = layout.box()
+            row = labelBox.row()
+            row.prop(label, "label", text="")
+            row.operator("image_mapper.remove_node_label", icon='X', text="").index = i
+        layout.operator("image_mapper.add_node_label", icon='ADD', text="Add Label")
+
         layout.label(text="Image Files:")
+        box = None 
         for i, image_file in enumerate(props.image_files):
-            box = layout.box()
+            if(not box):
+                box = layout.box()
             row = box.row()
+            row.prop(image_file, "deep", text="Recursive Search")
             row.prop(image_file, "file_path", text="")
             row.operator("image_mapper.remove_image_path", icon='X', text="").index = i
         
